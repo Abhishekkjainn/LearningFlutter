e1cbcaec3cf0711fd1d13b70245c6ad1ec1c8c61
abhishekkjainn <jainabhishek1904@gmail.com>
2023-09-30 22:48:19 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/abhishekapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.abhishekapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Material(
+            child: Center(
+      child: Container(
+        child: Text(
+          "This is My First App",
+          style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 30.0,
+              fontWeight: FontWeight.w600),
+        ),
+      ),
+    )));
+  }
+}
